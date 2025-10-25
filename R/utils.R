38c4c99187a5a97f25b31b3a5401781a56d5d374
Simon Couch <simonpatrickcouch@gmail.com>
2025-10-25 17:48:46 -0500

    relocate helper from confusingly-named file

--- R/utils.R
+++ R/utils.R
@@ -12,6 +12,23 @@ get_last_kick <- function() {
   NULL
 }
 
+tool_icon <- local({
+  icons <- list()
+  function(name) {
+    if (!is.null(icons[[name]])) {
+      return(icons[[name]])
+    }
+
+    icon <- htmltools::HTML(readLines(
+      system.file("icons", paste0(name, ".svg"), package = "side"),
+      warn = FALSE
+    ))
+    icons[[name]] <<- icon
+    return(icon)
+  }
+})
+
+
 # ad-hoc check functions ------------------------------------------------------
 check_inherits <- function(
   x,
